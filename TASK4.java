c76471f1ca30fa675bea69f61f803d4cd5a0e526
VITALIY1000 <vitalytyrenko1000@gmail.com>
2018-11-16 22:02:07 +0200

    TASK4 completed

---
+++ TASK4.java
@@ -0,0 +1,17 @@
+package ua.com.samsungitschool;
+
+class Main {
+
+    public static void main(String[] args) {
+        boolean result = isDivisibleThree(9);
+        System.out.println(result);
+    }
+
+    static boolean isDivisibleThree(int num) {
+        if (num % 3 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
