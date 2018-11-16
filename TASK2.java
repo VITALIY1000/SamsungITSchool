da2668415462b56d42969cbe8502c19f97e398e7
VITALIY1000 <vitalytyrenko1000@gmail.com>
2018-11-16 21:56:56 +0200

    TASK2 completed

---
+++ TASK2.java
@@ -0,0 +1,13 @@
+import ua.com.samsungitschool;
+
+class Main {
+
+    public static void main (String [] args) {
+        secondTask ();
+    }
+
+    public static void secondTask () {
+        float pi = 3.14159265358979f;
+        System.out.println ("PI =" + pi);
+    }
+}
