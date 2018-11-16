5bf9756eaa6fb9271350cc8fbe5e5933f960069d
VITALIY1000 <vitalytyrenko1000@gmail.com>
2018-11-16 21:59:55 +0200

    TASK3 completed

---
+++ TASK3.java
@@ -0,0 +1,12 @@
+package ua.com.samsungitschool;
+
+class Main {
+    
+    public static void main(String[] args) {
+        int result = math(1346, 36);
+    }
+
+    public static int math(int secondInt, int firstInteger) {
+        return secondInt + firstInteger;
+    }
+}
